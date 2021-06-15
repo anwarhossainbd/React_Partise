ef39f78e19155fa88ad2a48c82aa2b7d0916d394
unknown <mdanwarhossain1996bd@gmail.com>
2021-06-15 02:57:09 -0600

    anwar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import "./App.css"
 import Book from "./Component/Book";
 
 class App extends Component {
@@ -14,14 +15,41 @@ class App extends Component {
     }
   }
 
+    changeBookState=(firstbookName)=>{
+
+      this.setState({
+          books:[
+              { bookName:firstbookName, writter:"Tablig Jammat"},
+              { bookName:"Ahakame Jindigi", writter:"Mawlana Hemayet Ullah"},
+              { bookName:"Moroner Aga and Pore", writter:"Asraf Ali Thanvi"}
+          ]
+      })
+    }
+
+    changeWithInputState=(e)=>{
+
+        this.setState({
+            books:[
+                { bookName:e.target.value, writter:"Tablig Jammat"},
+                { bookName:"Ahakame Jindigi", writter:"Mawlana Hemayet Ullah"},
+                { bookName:"Moroner Aga and Pore", writter:"Asraf Ali Thanvi"}
+            ]
+        })
+    }
+
+
   render() {
     return (
-        <div>
-          <h1>welcome</h1>
+        <div className="App">
+          <h1>Book List</h1>
 
-          <Book bookName={this.state.books[0].bookName} writter={this.state.books[0].writter} />
+            <button onClick={this.changeBookState.bind(this,"1991")}>Change State</button>
+
+            <input type="text" onChange={this.changeWithInputState} />
+
+          <Book bookName={this.state.books[0].bookName} writter={this.state.books[0].writter} inputName={this.changeWithInputState} />
           <Book bookName={this.state.books[1].bookName} writter={this.state.books[1].writter} />
-          <Book bookName={this.state.books[2].bookName} writter={this.state.books[2].writter} />
+          <Book bookName={this.state.books[2].bookName} writter={this.state.books[2].writter} change={this.changeBookState.bind(this,"new life")} />
 
 
 
--- src/Component/Book.js
+++ src/Component/Book.js
@@ -5,6 +5,10 @@ const Book = (props) => {
         <div>
             <h3>Book: {props.bookName}</h3>
             <h4>Writer: {props.writter}</h4>
+
+            <input type="text" onChange={props.inputName} value={props.bookName}/> <br /> <br />
+
+            <button onClick={props.change}>Change</button>
         </div>
     );
 };
